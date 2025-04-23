068592fcda6bbb7bd8215fd43043b3d9372f6c2f
Kseniia Bondarenko <ksen9291@gmail.com>
2025-04-22 18:44:04 -0700

    fix: small bug fixes with the landing page

--- ai-library/ai-library.js
+++ ai-library/ai-library.js
@@ -1,52 +1,8 @@
 const allPodcastsContainer = document.querySelector('.all-podcasts-container');
 
-// const notebookLMpodcasts = [
-//     '../sounds/for-coding-NotebookLM-audio/attention-and-memory-notebook-LM-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/creativity-science-notebook-LM-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/fourth-dimension-notebookLM_podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/Paris-notebook-LM-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/Quantum-Mechanics-Notebook-LM.wav',
-
-//     // 
-//     '../sounds/for-coding-NotebookLM-audio/communication-skills-notebook-lm-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/Culture-of-the-United-States-notebook-lm-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/France-as-a-country-notebook-lm-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/France-culture-notebook-LM-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/French-cuisine-notebook-lm-podcast.wav',
 
 
-//     '../sounds/for-coding-NotebookLM-audio/Human-Brain-notebook-LM-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/jules-verne-around-the-world-in-80-days-notebook-lm-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/Language-learning-notebook-LM-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/longevity-notebook-lm-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/Louvre-notebook-LM-podcast.wav',
-
-
-//     '../sounds/for-coding-NotebookLM-audio/memory-and-learning-notebook-lm-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/memory-short-term-long-term-notebook-lm-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/musketeers-fiction-notebook-LM-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/Neuroscience-health-Notebook-LM_podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/notebook-LM-enchiridion-epictetus.wav',
-
-//     '../sounds/for-coding-NotebookLM-audio/Notebook-LM-history-of-Greece.wav',
-//     '../sounds/for-coding-NotebookLM-audio/Notebook-LM-The-Great-Pizza-Heist.wav',
-//     '../sounds/for-coding-NotebookLM-audio/Rousseau-confessions-notebook-LM.wav',
-//     '../sounds/for-coding-NotebookLM-audio/smartphone-science-notebook-LM-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/software-engineering-news-notebook-LM-podcast.wav',
-
-//     '../sounds/for-coding-NotebookLM-audio/Subatomic-particles-notebook-LM-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/Sweden-notebook-lm-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/United-States-as-a-country-notebook-lm-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/plant-and-tree-notebook-LM-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/sugar-notebook-LM-podcast.wav',
-
-//     '../sounds/for-coding-NotebookLM-audio/Milan-Italy-NotebookLM-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/Italian-culture-and-traditions-Notebook-LM-podcast.wav',
-//     '../sounds/for-coding-NotebookLM-audio/History-of-Italy-Notebook-LM-podcast.wav'
-
-// ];
-
-const notebookLMpodcasts = [
+const notebookLMpodcasts5 = [
    '../sounds/for-coding-NotebookLM-audio/attention-and-memory-notebook-LM-podcast.wav',
     '../sounds/for-coding-NotebookLM-audio/creativity-science-notebook-LM-podcast.wav',
     '../sounds/for-coding-NotebookLM-audio/fourth-dimension-notebookLM_podcast.wav',
@@ -93,7 +49,7 @@ function createAudioElement(src, index) {
 }
 
 function addAudioFiles() {
-    notebookLMpodcasts.forEach((podcast, index) => {
+    notebookLMpodcasts5.forEach((podcast, index) => {
         const audioElement = createAudioElement(podcast, index);
         allPodcastsContainer.appendChild(audioElement);
 
