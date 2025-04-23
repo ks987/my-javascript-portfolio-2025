c2d8e27498260e6f25e68e9fb578983074d493e4
Kseniia Bondarenko <ksen9291@gmail.com>
2025-04-22 17:40:55 -0700

    feat: mobile screen notepad styles

--- notepad/notepad.js
+++ notepad/notepad.js
@@ -1,27 +1,42 @@
-
+const notepadInput = document.querySelector('.notepad-title-input')
 const notepadTextarea = document.querySelector('.notepad-textarea');
 const saveButton = document.querySelector('.notepad-save-btn');
 const createdNotesBox = document.querySelector('.created-notes-box');
 
 
 const loadNotes = () => {
-    const notes = JSON.parse(localStorage.getItem("notepad-notes")) ?? [];
-    notes.forEach(createNoteElement);
+    const createdNotes = JSON.parse(localStorage.getItem("notepad-notes")) ?? [];
+    createdNotes.forEach(createNoteElement);
 }
 
 const createNoteElement = (content) => {
     const noteElementDiv = document.createElement('div');
     noteElementDiv.classList.add('one-note');
     noteElementDiv.textContent = content;
+
+    const noteTitle = document.createElement('h3');
+    noteTitle.classList.add('one-note-title');
+
+    const noteDescription = document.createElement('div');
+    noteDescription.classList.add('one-note-description');
+
+    noteElementDiv.appendChild(noteTitle);
+    noteElementDiv.appendChild(noteDescription);
     createdNotesBox.appendChild(noteElementDiv);
 }
 
 saveButton.addEventListener("click", () => {
-    const noteContent = notepadTextarea.value.trim();
+    const noteTitle = notepadInput.value.trim();
+    const noteDescription = notepadTextarea.value.trim();
+
+    const noteContent = `${noteTitle}: ${noteDescription}`;
     createNoteElement(noteContent);
+
+    notepadInput.value = "";
     notepadTextarea.value = "";
-    const notes = JSON.parse(localStorage.getItem("notes")) ?? [];
-    notes.push(noteContent);
+
+    const arrayOfNotes = JSON.parse(localStorage.getItem("notepad-notes")) ?? [];
+    arrayOfNotes.push(noteContent);
     localStorage.setItem("notepad-notes", JSON.stringify(notes));
 })
 
